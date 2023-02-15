f09a9bccd558e9fbb400987de2281d2cc71f4c17
Tequhella <lilian.chardon@gmail.com>
2023-02-15 13:04:42 +0100

    Utilisation de CMake

---
+++ Jeu-CPP/CMakeFiles/packages.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/packages"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/packages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
