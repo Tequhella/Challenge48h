ea2b64a20da5671f2360ec9a6a07c858c599ad51
Tequhella <lilian.chardon@gmail.com>
2023-02-15 13:20:03 +0100

    Update gitgnore

--- Jeu-CPP/CMakeFiles/packages.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/packages"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/packages.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
